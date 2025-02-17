ccb779ced84cb75b5e8c199afa5cc51925cafb4d
Flex1-tech <sethakplogan@gmail.com>
2025-02-17 15:01:05 +0100

    A tester

---
+++ second test/src/pages/Student/Explore.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {{ Link }} from 'react-router-dom';
+
+const Explore = () => {{
+    return (
+        <div>
+            <h1>Explore</h1>
+            <p>Cette page est en cours de conversion vers React TypeScript.</p>
+            <nav>
+                <ul>
+                    <li><Link to="/">Accueil</Link></li>
+                    <li><Link to="/admin/login">Connexion Admin</Link></li>
+                    <li><Link to="/admin/dashboard">Dashboard Admin</Link></li>
+                    <li><Link to="/student/login">Connexion Étudiant</Link></li>
+                    <li><Link to="/student/dashboard">Dashboard Étudiant</Link></li>
+                    <li><Link to="/student/explore">Explorer</Link></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}};
+
+export default Explore;
