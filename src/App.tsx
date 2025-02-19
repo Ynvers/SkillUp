7e664156124f924844769d613951c24da58993ba
Labyrinth-n1 <staradisso@gmail.com>
2025-02-19 19:18:16 +0100

    Correction des erreurs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import AdminDashboard from './pages/Admin/Dashboard'
 import AdminLogin from './pages/Admin/Login'
